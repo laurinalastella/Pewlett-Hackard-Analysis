cc19a3bbff54b9ba4bda59f9d37c4428cf6d2585
Laurina LaStella <106928784+laurinalastella@users.noreply.github.com>
2022-07-27 18:39:37 -0700

    7.3.1

--- schema.sql
+++ schema.sql
@@ -41,7 +41,7 @@ CREATE TABLE titles (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, from_date)
 );
 
 CREATE TABLE dept_emp (
@@ -51,7 +51,7 @@ CREATE TABLE dept_emp (
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, dept_no)
 );
 
 SELECT * FROM departments;
